86ee36f841241c385b57136ec7515e35a419a515
cynogriffin <89767958+cynogriffin@users.noreply.github.com>
2021-12-22 00:47:13 -0600

    added trash drop and styles to match the mockup

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,13 +44,6 @@ var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
-
-
-
-
-
-
-
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
   // clear values
